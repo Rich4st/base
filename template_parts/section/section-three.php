6da809d3baf5c0b648067a3b24757898fb50416d
linghu <1879194063@qq.com>
2023-08-09 16:18:11 +0800

    add:section-three

---
+++ template_parts/section/section-three.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 
+ * 
+ * @see https://github.com/Rich4st/base/blob/develop/preview/section-three.jpg?raw=true
+ * 
+ */
+?>
+<div class="flex flex-row lg:h-[550px]  lg:pt-10 h-64">
+    <img class="w-1/2 " src="<?php echo get_template_directory_uri(); ?>/assets/images/swiper_1.jpg" />
+    <div class=" w-1/2 bg-slate-200  flex flex-col items-center justify-center">
+        <div class="space-y-3 lg:py-20 text-center  flex flex-col">
+            <span class="font-mono  font-light text-3xl lg:text-5xl text-black ">Wonderful Landscape</span>
+            <span class="font-mono  font-light text-xl lg:text-3xl text-black">Look at the scenery</span>
+            <div>
+                <button class="btn btn-neutral w-24 ">
+                    <a href="/blog">
+                        Confirm
+                    </a>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
