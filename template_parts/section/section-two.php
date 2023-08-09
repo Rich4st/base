67890461ea085b15b54c9e2c679be38cd323e4b7
linghu <1879194063@qq.com>
2023-08-09 14:44:14 +0800

    add:section-two

---
+++ template_parts/section/section-two.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * 购物评价组件
+ * 
+ * @see https://github.com/Rich4st/base/blob/develop/preview/section-two.jpg?raw=true
+ * 
+ */
+?>
+<div class="flex lg:flex-col flex-col p-8 lg:p-10 font-[system-ui] ">
+    <div class="py-4 font-sans text-xl lg:text-3xl">Comments on our services</div>
+    <div class="flex lg:flex-row flex-col">
+        <div class="lg:w-1/3 py-4 lg:p-10">
+            <img class="rounded-lg bg-base-100 shadow-xl" src="<?php echo get_template_directory_uri(); ?>/assets/images/section_1.jpg" />
+            <div class="lg:text-3xl text-xl lg:py-8 py-4">Shopping Trolley</div>
+            <div class="text-gray-400">Customer service will contact you later in the same wayPlease add your favorite items to the shopping cart to take home</div>
+        </div>
+        <div class="lg:w-1/3 py-4 lg:p-10 ">
+            <img class="rounded-lg bg-base-100 shadow-xl" src="<?php echo get_template_directory_uri(); ?>/assets/images/section_2.jpg" />
+            <div class="lg:text-3xl text-xl lg:py-8 py-4">Telegram Praise</div>
+            <div class="text-gray-400">I commend you for your service</div>
+        </div>
+        <div class="lg:w-1/3 py-4 lg:p-10 ">
+            <img class="rounded-lg bg-base-100 shadow-xl" src="<?php echo get_template_directory_uri(); ?>/assets/images/section_3.jpg" />
+            <div class="lg:text-3xl text-xl lg:py-8 py-4">Express delivery</div>
+            <div class="text-gray-400">We'll make every effort to hasten the delivery of the goods</div>
+        </div>
+    </div>
+
+</div>
