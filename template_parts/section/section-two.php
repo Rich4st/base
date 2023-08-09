2417e2ae596839e603d8d1ffe39b2f3e679d2464
linghu <1879194063@qq.com>
2023-08-09 15:05:30 +0800

    change:section-two

--- template_parts/section/section-two.php
+++ template_parts/section/section-two.php
@@ -7,20 +7,20 @@
  * 
  */
 ?>
-<div class="flex lg:flex-col flex-col p-8 lg:p-10 font-[system-ui] ">
+<div class="flex flex-col p-8 lg:p-10 font-[system-ui] ">
     <div class="py-4 font-sans text-xl lg:text-3xl">Comments on our services</div>
-    <div class="flex lg:flex-row flex-col">
-        <div class="lg:w-1/3 py-4 lg:p-10">
+    <div class="flex lg:flex-row flex-col space-y-4">
+        <div class="lg:w-1/3 lg:p-10">
             <img class="rounded-lg bg-base-100 shadow-xl" src="<?php echo get_template_directory_uri(); ?>/assets/images/section_1.jpg" />
             <div class="lg:text-3xl text-xl lg:py-8 py-4">Shopping Trolley</div>
             <div class="text-gray-400">Customer service will contact you later in the same wayPlease add your favorite items to the shopping cart to take home</div>
         </div>
-        <div class="lg:w-1/3 py-4 lg:p-10 ">
+        <div class="lg:w-1/3 lg:p-10 ">
             <img class="rounded-lg bg-base-100 shadow-xl" src="<?php echo get_template_directory_uri(); ?>/assets/images/section_2.jpg" />
             <div class="lg:text-3xl text-xl lg:py-8 py-4">Telegram Praise</div>
             <div class="text-gray-400">I commend you for your service</div>
         </div>
-        <div class="lg:w-1/3 py-4 lg:p-10 ">
+        <div class="lg:w-1/3 lg:p-10 ">
             <img class="rounded-lg bg-base-100 shadow-xl" src="<?php echo get_template_directory_uri(); ?>/assets/images/section_3.jpg" />
             <div class="lg:text-3xl text-xl lg:py-8 py-4">Express delivery</div>
             <div class="text-gray-400">We'll make every effort to hasten the delivery of the goods</div>
