0236b09148aa91886cfcbd04bad14c30ca6bbce6
Rich4st <zmingcheng204@gmail.com>
2023-08-09 17:22:25 +0800

    fix: error

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/swiper-bundle.min.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
   <script src="<?php echo get_template_directory_uri(); ?>/assets/js/index.js"></script>
-  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/pagination.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/assets/js/swiper-bundle.min.js"></script>
   <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
   <?php wp_head(); ?>
