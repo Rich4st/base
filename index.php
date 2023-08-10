eb60f28a939369f259f88ded628852e6944b7932
Rich4st <zmingcheng204@gmail.com>
2023-08-10 09:25:30 +0800

    chore: code structure refine

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 <main>
 
-  <h1>Content</h1>
+  <section>
+    <?php get_template_part('template_parts/section/section-7') ?>
+  </section>
 
 </main>
 
--- template_parts/section/section-2.php
+++ template_parts/section/section-2.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
+
 /**
  * @description 轮播图组件
  * 
  * @see https://github.com/Rich4st/base/blob/develop/preview/section-2.jpg?raw=true
  */
 ?>
+
 <div class="swiper mySwiper rounded-xl overflow-auto">
   <div class="swiper-wrapper">
     <div class="swiper-slide">
@@ -40,3 +42,27 @@
   </div>
   <div class="swiper-pagination"></div>
 </div>
+
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/swiper-bundle.min.js"></script>
+
+<script>
+  if (Swiper) {
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+</script>
