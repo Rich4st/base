ac0c499911057c0bad993e43caacf32b6da3e4d0
Rich4st <zmingcheng204@gmail.com>
2023-08-10 16:09:09 +0800

    feat: add section-9

---
+++ template_parts/section/section-9.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * 
+ * @description FAQ组件
+ * 
+ * @see https://github.com/Rich4st/base/blob/develop/preview/section-9.jpg?raw=true
+ * 
+ */
+?>
+<div class="font-sans overflow-hidden">
+
+  <div class="h-32 flex items-center justify-center bg-indigo-500">
+    <h3 class="text-5xl lg:text-6xl font-meshed">FAQ</h3>
+  </div>
+
+  <div class="wrapper border-b-2 -mt-8 bg-white overflow-hidden mx-auto max-w-md rounded shadow-lg">
+
+    <h3 class="bg-grey-lightest px-8 py-6 font-semibold">Frequently Asked Questions</h3>
+
+    <div class="question-wrap mx-8 mt-2">
+      <details class="question py-4 border-b border-grey-lighter">
+
+        <summary class="flex items-center font-bold">My childhoood
+          <button class="ml-auto">
+            <svg class="fill-current opacity-75 w-4 h-4 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+            </svg>
+          </button>
+        </summary>
+
+        <div class="mt-4 leading-normal text-md ">I lived near lake Eerie and I really miss sunsets over the water. Fuga perspiciatis quidem sunt animi. We can all grab at the chance to be handsome farmers. YEAH you can have 21 sons that'll be blood when they marry my daughters. And the pain that we left at the station will stay in a jar behind us. We can pickle the pain into blue ribbon winners at county contests....
+          Gosh. I loved her to bits</div>
+      </details>
+
+      <details class="question py-4 border-b border-grey-lighter">
+
+        <summary class="flex items-center">Ex nihilo nihil
+          <button class="ml-auto">
+            <svg class="fill-current opacity-75 w-4 h-4 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+            </svg>
+          </button>
+        </summary>
+
+        <div class="mt-4 leading-normal text-md">
+          <p>ništavilo se vraća sebi kao sve u svemu</p>
+        </div>
+      </details>
+
+      <details class="question py-4 border-b border-grey-lighter">
+
+        <summary class="flex items-center">Estuarij iznad rijeke [r]iverice
+          <button class="ml-auto">
+            <svg class="fill-current opacity-75 w-4 h-4 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+            </svg>
+          </button>
+        </summary>
+
+        <div class="mt-4 leading-normal text-md ">Zašto pitaš mene? Pitaj šulasice. Question remains - should I invest in a bycicle? !
+          Estuarij iznad rijeke Iverice [riverice].</div>
+      </details>
+
+
+      <details class="question py-4 border-b border-grey-lighter">
+
+        <summary class="flex items-center font-bold">Poput oceana tekuće magije
+          <button class="ml-auto">
+            <svg class="fill-current opacity-75 w-4 h-4 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+            </svg>
+          </button>
+        </summary>
+
+        <div class="mt-4 leading-normal text-md ">
+          Čitav naš svijet je poput oceana tekuće materije. Svi njegovi rubovi i rubovi objekata koje percipiramo kao objekte s čvrstim stjenkama samo su iluzija koju nam nameću naša ograničena osjetila. Odnosno, naša osjetila ili kakav god drugi instrumentarij izmislili kako bismo proširili područje vlastita opažanja, uvijek će ostati nedovoljno savršena i ograničena za druge dimenzije svijesti, dimenzije koje nadilaze materijalni svijet, prostor i vrijeme. No...zađemo li malo dublje u smisao i bit ljudskog postojanja shvatiti ćemo da postoje i drugi načini komunikacije i opažanja kudikamo suptilniji od onih kojeg čovjek može dosegnuti s svojih pet čula, a to su oni putem univerzalne svijesti. Naime, mi kao ljudska bića nismo izolirani od ostatka svijeta i svemira. Uronjen u nevidljivu vibraciju polja ovog svemira čovjek je smisleno povezan s svime što ga okružuje i s svime što postoji i što je ikada postojalo, a što dolazi iz istog praiskonskog izvora...pa na jednoj dubljoj, duhovnijoj i svjesnijoj razini čovjek ipak može osjetiti tu vibraciju. Čitav naš svijet je poput oceana tekuće materije. Svi njegovi rubovi i rubovi objekata koje percipiramo kao objekte s čvrstim stjenkama samo su iluzija koju nam nameću naša ograničena osjetila. Odnosno, naša osjetila ili kakav god drugi instrumentarij izmislili kako bismo proširili područje vlastita opažanja, uvijek će ostati nedovoljno savršena i ograničena za druge dimenzije svijesti, dimenzije koje nadilaze materijalni svijet, prostor i vrijeme. No...zađemo li malo dublje u smisao i bit ljudskog postojanja shvatiti ćemo da postoje i drugi načini komunikacije i opažanja kudikamo suptilniji od onih kojeg čovjek može dosegnuti s svojih pet čula, a to su oni putem univerzalne svijesti. Naime, mi kao ljudska bića nismo izolirani od ostatka svijeta i svemira. Uronjen u nevidljivu vibraciju polja ovog svemira čovjek je smisleno povezan s svime što ga okružuje i s svime što postoji i što je ikada postojalo, a što dolazi iz istog praiskonskog izvora...pa na jednoj dubljoj, duhovnijoj i svjesnijoj razini čovjek ipak može osjetiti tu vibraciju.
+        </div>
+      </details>
+    </div>
+  </div>
+</div>
