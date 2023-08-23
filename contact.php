38073cdd7cc33f0ab783964e555a45be7d7c3ee6
Rich4st <zmingcheng204@gmail.com>
2023-08-23 11:48:43 +0800

    feat: page seo

--- contact.php
+++ contact.php
@@ -2,8 +2,31 @@
 /**
  * Template Name: Contact
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-get_header(); ?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?> | <?php the_title() ?></title>
+  <meta name="description" content="capalot's blog">
+  <meta name="keywords" content="blog,博客">
+  <meta name="og:title" content="<?php bloginfo('name'); ?>-<?php the_title() ?>">
+  <meta name="og:description" content="capalot's blog">
+  <meta name="og:keywords" content="blog,博客">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?php bloginfo('url') ?>">
+  <meta property="og:site_name" content="<?php bloginfo('name') ?>">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/tailwind.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/swiper-bundle.min.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/animate.min.css">
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/index.js"></script>
+  <?php wp_head(); ?>
+</head>
+
+<?php get_header(); ?>
 
 <main class="lg:max-w-6xl lg:mx-auto">
 
--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?> | <?php the_title() ?></title>
+  <meta name="description" content="capalot's blog">
+  <meta name="keywords" content="blog,博客">
+  <meta name="og:title" content="<?php bloginfo('name'); ?>-<?php the_title() ?>">
+  <meta name="og:description" content="capalot's blog">
+  <meta name="og:keywords" content="blog,博客">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?php bloginfo('url') ?>">
+  <meta property="og:site_name" content="<?php bloginfo('name') ?>">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/tailwind.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/swiper-bundle.min.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/animate.min.css">
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/index.js"></script>
+  <?php wp_head(); ?>
+</head>
+
 <?php get_header(); ?>
 
 <main>
