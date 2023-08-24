9a03016f5d15d99a6964224600893303dbbbb7f2
Rich4st <zmingcheng204@gmail.com>
2023-08-24 11:38:11 +0800

    feat: add section-11

---
+++ template_parts/section/section-11.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @description 首页heropage
+ * 
+ * @see https://github.com/Rich4st/base/blob/develop/preview/section-11.jpg?raw=true
+ */
+?>
+<div class="font-pop text-center">
+  <lord-icon src="https://cdn.lordicon.com/ihyatngg.json" trigger="hover" colors="primary:#4be1ec,secondary:#cb5eee" style="width:250px;height:250px">
+  </lord-icon>
+  <h1 class="text-5xl lg:text-7xl font-bold">
+    INTERGALACITIC2
+  </h1>
+  <p class="text-lg lg:text-2xl text-gray-600">Out of this world</p>
+</div>
+<div class="relative font-pop">
+  <img class="w-full h-[30rem] object-cover" src="<?php echo get_template_directory_uri() ?>/assets/images/astronaut.jpg" alt="hero image">
+  <div class="absolute inset-0 bg-black opacity-50 z-10"></div>
+  <div class="text-white z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+    <h2 class="text-3xl lg:text-4xl font-bold mb-8">WELCOME,SPACE TRAVELERS</h2>
+    <p class="max-w-xl text-left mb-6">Welcome to our voyage,we're going to have blast!Join us as we make our way throughout the galaxy.</p>
+    <a class="px-10 py-2 outline outline-1 hover:bg-black hover:outline-none duration-200" href="/blog" title="blog">READ MORE</a>
+  </div>
+</div>
